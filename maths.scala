49ee40e9bc56770b7bf89453e52b196f9b331f50
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-02 16:33:24 +0100

    Create maths.scala

---
+++ maths.scala
@@ -0,0 +1,30 @@
+
+// Scala program to demonstrate
+// the Arithmetic Operators
+ 
+object Arithop
+{
+ 
+def main(args: Array[String])
+{
+    // variables
+    var a = 50;
+    var b = 30;
+     
+    // Addition
+    println("Addition of a + b = " + (a + b));
+     
+    // Subtraction
+    println("Subtraction of a - b = " + (a - b));
+     
+    // Multiplication
+    println("Multiplication of a * b = " + (a * b));
+     
+    // Division
+    println("Division of a / b = " + (a / b));
+     
+    // Modulus
+    println("Modulus of a % b = " + (a % b));
+ 
+}
+}
