cd590e9e289fdceef96bb5b2f5e18c14e2b18027
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-02 16:32:27 +0100

    Create hello world.scala

---
+++ hello world.scala
@@ -0,0 +1,11 @@
+
+// Scala program to print Hello World! 
+object eggy115 
+{
+    // Main Method 
+    def main(args: Array[String]) 
+    {
+        // prints Hello World
+        println("Hello World!") 
+    }
+}
